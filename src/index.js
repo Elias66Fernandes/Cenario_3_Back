d0ee6aa119645d385fb567c30af1e4e31d7b770d
Elias66Fernandes <francisco.elias@discente.ufma.br>
2023-12-02 09:50:57 -0300

    Testes 03

--- src/index.js
+++ src/index.js
@@ -1,3 +1,86 @@
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const app = express();
+const port = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  if (users.length > 0) return res.status(200).send(users);
+  return res.status(404).send("No users found");
+});
+
+app.post("/user", async (req, res) => {
+  const data = req.body;
+  await prisma.user.create({
+    data: {
+      name: data.name,
+      email: data.email,
+    },
+  });
+  return res.sendStatus(201);
+});
+
+app.delete("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  await prisma.user.delete({
+    where: {
+      id: Number(id),
+    },
+  });
+  return res.sendStatus(200);
+});
+
+app.put("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  await prisma.user.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      name: data.name,
+      email: data.email,
+    },
+  });
+  return res.sendStatus(200);
+});
+
+app.get("/users/:name", async (req, res) => {
+  const name = req.params.name;
+  const user = await prisma.user.findMany({
+    where: {
+      name: name,
+    },
+  });
+  if (user.length > 0) return res.status(200).send(user);
+  return res.status(404).send("No users found");
+});
+
+app.get("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await prisma.user.findMany({
+    where: {
+      id: Number(id),
+    },
+  });
+  if (user.length > 0) return res.status(200).send(user);
+  return res.status(404).send("No user found");
+});
+
+// Inicie o servidor na porta especificada
+const server = app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
+
+module.exports = { app, server };
+
+/*
 //importações necessárias para o projeto
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
@@ -28,7 +111,7 @@ app.get("/users", async (req, res) => {
     console.error("Erro ao listar todos os usuários cadastrados", error);
   }
 });
-/*
+
 //rota que cadastra um usuário
 app.post("/users", async (req, res) => {
   try {
@@ -179,10 +262,10 @@ app.get("/users/:id", async (req, res) => {
     console.error('Erro ao buscar usuário.', error);
   }
 });
-*/
+
 // Inicie o servidor na porta especificada
 const server = app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
 
-module.exports = { app, server };
+module.exports = { app, server };*/
